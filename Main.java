e54128b2190b0af7939e1921386397bb48519af4
Desiree Lopez <91097849@g.mvusd.net>
2021-01-11 20:19:13 +0000

    added line 11

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ class Main
     int num1;
     System.out.println("Pick an int:");
     num1 = kb.nextInt();
+    System.out.println("Today's lucky number is " + num1);
   }//end main method
 }//close the main class 
