1dd78821e6639c849dedc75261c8b0c801289330
Desiree Lopez <91097849@g.mvusd.net>
2021-01-11 20:02:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner kb = new Scanner (System.in);
+    int num1;
+    System.out.println("Pick an int:");
+    num1 = kb.nextInt();
+  }//end main method
+}//close the main class 
